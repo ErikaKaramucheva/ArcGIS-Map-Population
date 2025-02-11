64a3d29a85f513f8a09ddb093841b5f8c20b9eb3
ErikaKaramucheva <105073095+ErikaKaramucheva@users.noreply.github.com>
2025-02-11 11:43:55 +0200

    Add files via upload
    init

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ArcGISMap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPTxy8BH1VEsoebNVZXo8HurNGhKSq4YwbtCpzff9VKuNAipFWZbqToTnvSHJNAVIjoorMAjRFzpTtwYGOC425jbSM-43_yqYkkk_kCiIQfWKdFUM5LPpX8c-qF4_MtqB3nHk5WQFsrCW2fLyodZzWaW7yjd91StcO1IwiBlFLqyvxV0rNonBf8m_FFGu_gl9SJVdBDhLAeWfdBqdxSwT1ONPEhgmqmndMhlYJf3DBdNLM.AT1_HbjMNBgs";
+        }
+    }
+
+}
---
+++ ViewModels/MapViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ArcGISMap.ViewModels
+
+{
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MapViewModel() { SetupMap(); }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map? map;
+        public Map? Map
+        {
+            get { return map; }
+            set
+            {
+                map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetupMap()
+        {
+            // create a map with a topographic vector basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+        }
+    }
+    
+}
